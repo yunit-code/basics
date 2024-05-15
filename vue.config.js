c48909dfb387c38163fa3f3df293b0b2bc2a1fb2
chensq <chensq@dreamit.cn>
2024-05-15 14:44:25 +0800

    修改打包后文件数量

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 
 const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack');
+const isDev = process.env.NODE_ENV === 'development'
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
@@ -48,6 +49,7 @@ const externals = {
 }
 module.exports = {
     publicPath:"./",
+    productionSourceMap: false,
     assetsDir:assetsDir,
     pages:{
       index: {
@@ -115,16 +117,19 @@ module.exports = {
     },
     configureWebpack: {
       plugins: [
-        new MiniCssExtractPlugin({
-          // 修改打包后css文件名
-          filename: `${assetsDir}/css/[name].css`,
-          chunkFilename: `${assetsDir}/css/[name].css`
-        })
+        new webpack.optimize.LimitChunkCountPlugin({
+          maxChunks: 1
+        }),
+        // new MiniCssExtractPlugin({
+        //   // 修改打包后css文件名
+        //   filename: `${assetsDir}/css/[name].css`,
+        //   chunkFilename: `${assetsDir}/css/[name].css`
+        // })
       ],
       output: {
         // 输出重构  打包编译后的 文件名称
         filename: `${assetsDir}/js/[name].js`,
-        chunkFilename: `${assetsDir}/js/[name].js`,
+        // chunkFilename: `${assetsDir}/js/[name].js`,
         jsonpFunction:JSON.stringify("webpackJsonp_"+getGUID()+"_"+new Date().getTime())
       },
       resolve:{
@@ -137,9 +142,12 @@ module.exports = {
     },
     css: {
         // 是否使用css分离插件 ExtractTextPlugin
-        extract: true,
+        extract: isDev ? false : {
+          filename: `${assetsDir}/css/[name].css`,
+          // chunkFilename: `${assetsDir}/css/[name].css`
+        },
         // 开启 CSS source maps?
-        sourceMap: true,
+        sourceMap: isDev,
         // css预设器配置项
         // 启用 CSS modules for all css / pre-processor files.
         requireModuleExtension: true,
